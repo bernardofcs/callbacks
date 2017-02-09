b2c0ab09556b217515230d452c1ed9748a9786fd
Bernardo Siqueira <bfcsiqueira@gmail.com>
2017-02-09 04:13:01 +0000

    updated function to use forEach

--- callbacks-introduction.js
+++ callbacks-introduction.js
@@ -1,13 +1,13 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(name, index) {
+    if (name === "Waldo") {
+      found(index);   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {
-  console.log('Found him at index' + index + '!');
+  console.log('Found him at index ' + index + '!');
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
